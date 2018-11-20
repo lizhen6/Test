a514557a1a35472f6bbcf109c853cee5453427b8
lizhen <lizhen7@asiainfo.com>
2018-11-20 13:38:41 +0800

    11

---
+++ src/main/java/com/lz/demo/HelloController.java
@@ -0,0 +1,24 @@
+package com.lz.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 李镇
+ * @data 2018/11/17 14:55
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/xuanxuan")
+    public String getHello(){
+        System.out.println("111111");
+        return "/love";
+    }
+
+    @RequestMapping("/lz")
+    public String getLz(){
+        System.out.println("222");
+        return "/biaobai";
+    }
+}
